4a71fd35c571c1b292169e27067f2c0c979eafb7
Felipe Santos <silva93felipe@gmail.com>
2024-08-09 08:39:29 -0300

    Inicial

--- ApiIntegracao/Queue/Consumer.cs
+++ ApiIntegracao/Queue/Consumer.cs
@@ -1,33 +1,33 @@
 using System.Text;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+// using RabbitMQ.Client;
+// using RabbitMQ.Client.Events;
 
 namespace ApiIntegracao.Queue
 {
     public class Consumer : BackgroundService, IBus{
-        private readonly IConnection _connectionRabbitmq;
-        private readonly IModel _channelRabbitmq;
+        // private readonly IConnection _connectionRabbitmq;
+        // private readonly IModel _channelRabbitmq;
         public Consumer()
         {
-            var factory = new ConnectionFactory
-            {
-                HostName = "localhost"
-            };
-            _connectionRabbitmq =  factory.CreateConnection();
-            _channelRabbitmq  = _connectionRabbitmq.CreateModel();
-            _channelRabbitmq.QueueDeclare("create_pedido", false, false, false, null);
+            // var factory = new ConnectionFactory
+            // {
+            //     HostName = "localhost"
+            // };
+            // _connectionRabbitmq =  factory.CreateConnection();
+            // _channelRabbitmq  = _connectionRabbitmq.CreateModel();
+            // _channelRabbitmq.QueueDeclare("create_pedido", false, false, false, null);
         }
 
         public void BasicConsume()
         {
-            var consumer = new EventingBasicConsumer(_channelRabbitmq);
-            consumer.Received += (sender, eventArgs) => 
-            {                
-                var contentArray = eventArgs.Body.ToArray();
-                var contentString = Encoding.UTF8.GetString(contentArray);
-                Console.WriteLine("Lido " + contentString);
-            };
-            _channelRabbitmq.BasicConsume("create_pedido", false, consumer);
+            // var consumer = new EventingBasicConsumer(_channelRabbitmq);
+            // consumer.Received += (sender, eventArgs) => 
+            // {                
+            //     var contentArray = eventArgs.Body.ToArray();
+            //     var contentString = Encoding.UTF8.GetString(contentArray);
+            //     Console.WriteLine("Lido " + contentString);
+            // };
+            // _channelRabbitmq.BasicConsume("create_pedido", false, consumer);
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
--- ApiPedidos/Infra/Queue/Producer.cs
+++ ApiPedidos/Infra/Queue/Producer.cs
@@ -3,7 +3,7 @@
 using System.Text;
 using ApiPedidos.Contratos;
 using Newtonsoft.Json;
-using RabbitMQ.Client;
+// using RabbitMQ.Client;
 
 namespace ApiPedidos.Queue
 {
@@ -11,22 +11,22 @@ namespace ApiPedidos.Queue
 
         public void Publish<T>(T message)
         {
-            var factory = new ConnectionFactory() { 
-                HostName = "localhost", 
-                // Port = 5672,
-                // UserName="guest", 
-                // Password="guest" 
-            };
-            var connection = factory.CreateConnection();
-            var channel = connection.CreateModel();
-            channel.QueueDeclare(queue: "create_pedido",
-                                    durable: false,
-                                    exclusive: false,
-                                    autoDelete: false,
-                                    arguments: null);
-            var json = JsonConvert.SerializeObject(message);
-            var body = Encoding.UTF8.GetBytes(json);
-            channel.BasicPublish("", "create_pedido", null, body);
+            // var factory = new ConnectionFactory() { 
+            //     HostName = "localhost", 
+            //     // Port = 5672,
+            //     // UserName="guest", 
+            //     // Password="guest" 
+            // };
+            // var connection = factory.CreateConnection();
+            // var channel = connection.CreateModel();
+            // channel.QueueDeclare(queue: "create_pedido",
+            //                         durable: false,
+            //                         exclusive: false,
+            //                         autoDelete: false,
+            //                         arguments: null);
+            // var json = JsonConvert.SerializeObject(message);
+            // var body = Encoding.UTF8.GetBytes(json);
+            // channel.BasicPublish("", "create_pedido", null, body);
         }
     }
 }
